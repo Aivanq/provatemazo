012dde2bd7a9b87b32ced7aa9f1a843755f4cfc2
Iván Quero <ivan.quero@iesjoandustria.org>
2019-07-03 11:23:49 +0200

    3r modificat

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM alpine:3.9.4
 RUN apk add curl \
+&&  apk add mplayer \
 &&  apk add ffmpeg \
+&&  apk add python \
 &&  curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl \
-&&  chmod a+rx /usr/local/bin/youtube-dl
+&&  chmod a+rx /usr/local/bin/youtube-dl  \
+&& mkdir /music
+
 COPY temazomod.sh /usr/local/bin
-#ENTRYPOINT ["/usr/local/bin/temazomod.sh"]
-#CMD ["thunderstruck","seagulls"]
+
+ENTRYPOINT ["/usr/local/bin/temazomod.sh"]
+CMD ["thunderstruck","seagulls"]
--- temazomod.sh
+++ temazomod.sh
@@ -1,11 +1,13 @@
-#!/bin/bash 
+#!/bin/sh 
 #https://www.youtube.com/results?search_query=thunderstruck+seagulls
-
+MUSICDIR=/music
 quebusco=$(echo $*  | tr " " "+" )
 urlachupar=$(curl -s "https://www.youtube.com/results?search_query=$quebusco"| grep -m1 "/watch" | sed "sx.*href=\"/watchxhttps://www.youtube.com/watchx" | cut -d \" -f1)
+r
 
-FILENAME="$(youtube-dl -x --audio-format mp3 --audio-quality 0 "$urlachupar"  |& fgrep '[ffmpeg] Destination: ' | sed "s/^\[ffmpeg\] Destination: //" )"
-echo $FILENAME
+FILENAME="$(youtube-dl -x --audio-format mp3 --audio-quality 0 "$urlachupar"  | fgrep '[ffmpeg] Destination: ' | sed "s/^\[ffmpeg\] Destination: //" )"
+
 NEWFILENAME="$( echo $FILENAME | sed "s/-[^-]\+\.mp3/.mp3/" )"
-mv "$FILENAME" "$NEWFILENAME"
-mplayer "$NEWFILENAME"
+mv "$FILENAME" "$MUSICDIR/$NEWFILENAME"
+
+mplayer "$MUSICDIR/$NEWFILENAME"
